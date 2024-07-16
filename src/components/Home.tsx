a648f8ec1b43a7b563a652086da7d6c244fa943b
senthildevel <132339613+senthildevel@users.noreply.github.com>
2024-07-16 18:17:28 +0530

    Added title and updated home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import bannerImg from "../assets/bridge.jpg";
 export const Home = () => {
   return (
     <>
+      <h2 className="mb-3">Home Page</h2>
       <img src={bannerImg} alt="" className="w-100" height="450" />
     </>
   );
